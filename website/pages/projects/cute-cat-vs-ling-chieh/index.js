db2853e6a88594065f54f4b6c3541a1c43f48d56
AngusLee <hsushen.lee@mail.utoronto.ca>
2022-09-22 15:10:02 -0400

    attempt to fix deploy bug 3

---
+++ website/pages/projects/cute-cat-vs-ling-chieh/index.js
@@ -0,0 +1,15 @@
+import {
+  Center
+} from "@chakra-ui/react";
+
+const CuteCatVsLingChieh = () => {
+  return (
+    <>
+      <Center>
+        under construction
+      </Center>
+    </>
+  );
+};
+
+export default CuteCatVsLingChieh;
